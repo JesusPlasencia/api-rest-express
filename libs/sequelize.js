d5dfaaa0b456e145c507234fadab9bb6a7df45fa
Jesus Plasencia Toledo <jesusplasencia2508@hotmail.com>
2022-08-26 18:09:56 -0500

    9C - Migrations

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -4,15 +4,15 @@ const setUpModels = require('../db/models/index');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 const sequelize = new Sequelize(URI, {
-  dialect: 'mysql',
+  dialect: 'postgres',
   logging: true,
 });
 
 setUpModels(sequelize);
 
-sequelize.sync();
+// sequelize.sync();
 
 module.exports = { sequelize };
