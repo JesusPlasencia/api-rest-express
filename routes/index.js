d788709fcf0f02769986086984b31e73c0964d97
Jesus Plasencia Toledo <jesusplasencia2508@hotmail.com>
2022-08-26 09:54:57 -0500

    6C - Docker

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const productsRouter = require('./products');
-const usersRouter = require('./user');
-const categoriesRouter = require('./categories');
+const productsRouter = require('./product.router');
+const usersRouter = require('./user.router');
+const categoriesRouter = require('./category.router');
 
 function routerAPI(app) {
   const router = express.Router();
