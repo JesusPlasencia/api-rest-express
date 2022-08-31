2eab944217f5e4511bff5cc07f5f8b3ac1d3220f
Jesus Plasencia Toledo <jesusplasencia2508@hotmail.com>
2022-08-31 17:00:15 -0500

    18C - Fix migrations

--- db/migrations/20220830221147-add-role.js
+++ db/migrations/20220830221147-add-role.js
@@ -1,11 +1,16 @@
 'use strict';
 
-const { UserSchema, USER_TABLE } = require('../models/user.model');
+const { USER_TABLE } = require('../models/user.model');
+const { DataTypes } = require('sequelize');
 
 module.exports = {
   async up(queryInterface) {
     //Adding role column to User's table | function addColumn()
-    await queryInterface.addColumn(USER_TABLE, 'role', UserSchema.role);
+    await queryInterface.addColumn(USER_TABLE, 'role', {
+      allowNull: false,
+      type: DataTypes.STRING,
+      defaultValue: 'Customer',
+    });
   },
 
   async down(queryInterface) {
